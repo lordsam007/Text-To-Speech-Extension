bbade75c7991c22b78ab9dc725f29a02eb84ef57
lordsam007 <samdevelops7@gmail.com>
2023-09-17 18:28:53 +0530

    Text to speech extension

---
+++ popup.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+  const speakButton = document.getElementById('speak-button');
+  const textToSpeak = document.getElementById('text-to-speak');
+
+  speakButton.addEventListener('click', function () {
+    const text = textToSpeak.value;
+
+    if (text) {
+      speakText(text);
+    }
+  });
+
+  // Function to speak text using the Web Speech API
+  function speakText(text) {
+    const speechSynthesis = window.speechSynthesis;
+    const speechText = new SpeechSynthesisUtterance(text);
+
+    speechSynthesis.speak(speechText);
+  }
+});
